ae76d14f7254a78fb30a36bd031e1ca7f8c28af6
uae004 <54192068+uae004@users.noreply.github.com>
2019-08-19 02:01:30 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(" 612071544226578473")
+setInterval(function() {
+channel.send(`abo mohamed`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
